f0f098f9ede65eecf7f32ee5045bae5bcf1adb22
klekkwedge <antonov.andrey.10101@gmail.com>
2022-07-11 10:08:08 +0300

    feat: add basic project structure

---
+++ unit-calculator/src/components/app/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Box, Text, Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Box bg="tomato">
+        <Text fontSize="2xl">Hello!</Text>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
---
+++ unit-calculator/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/app/App";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
