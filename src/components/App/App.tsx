690c58766107f8c37618d4c95a41e08948a72d86
Andrey <antonov.andrey.10101@gmail.com>
2023-08-16 11:58:42 +0400

    refactor: remove unused components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,10 @@
 import { Box } from '@chakra-ui/react';
-import Converter from '../Converter/Converter';
 import CustomNumberInput from '../CustomNumberInput/CustomNumberInput';
 
 function App() {
   return (
     <Box h="90vh">
       <CustomNumberInput />
-      <Converter />
     </Box>
   );
 }
